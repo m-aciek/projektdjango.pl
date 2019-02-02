0abfdd4710d0fe6fcaf2ac2018ec49d1abd95d39
Maciej Olko <maciej.olko@gmail.com>
2019-02-02 14:49:59 +0100

    Add Bulma.

--- tasks.py
+++ tasks.py
@@ -4,6 +4,6 @@ from settings import DEPLOY_TARGET
 
 @task
 def deploy(c):
-    c.run(f'rsync --dry-run -avz index.html {DEPLOY_TARGET}')
+    c.run(f'rsync --dry-run -avz index.html static {DEPLOY_TARGET}')
     if input('Are you sure you want to deploy?') == 'yes':
         c.run(f'rsync -avz index.html {DEPLOY_TARGET}')
